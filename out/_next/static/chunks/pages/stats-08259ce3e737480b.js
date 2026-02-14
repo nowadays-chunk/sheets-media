6a8ae4922c3111e626a888b3e58f44154b50f85d
Hamza Eljaouhari <h.eljaouhari.home@gmail.com>
2026-02-14 18:11:27 +0100

    Deleted unecessary folder (out, .next and pdf and png sreens)

--- out/_next/static/chunks/pages/stats-08259ce3e737480b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[1833],{56164:(_,s,e)=>{"use strict";e.r(s),e.d(s,{__N_SSG:()=>u,default:()=>n});var t=e(87174),u=!0;let n=t.A},69473:(_,s,e)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/stats",function(){return e(56164)}])}},_=>{_.O(0,[1316,5812,4793,636,6593,6411],()=>_(_.s=69473)),_N_E=_.O()}]);
